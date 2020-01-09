7f2c675ce86b1bc3b9f50bcda789213929531039
qianyin <qianyin@newhope.cn>
2020-01-09 22:13:21 +0800

    update

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post,Category,Tag
 # Register your models here.
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'create_time')
