8b74fae246ace626b9e0acce5a651f490f8f3c93
qianyin <qianyin@newhope.cn>
2020-01-09 22:30:46 +0800

    fix Exception Value: NOT NULL constraint failed: blog_category.owner_id

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -8,8 +8,16 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'create_time')
     fields = ('name', 'status', 'is_nav')
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'create_time')
     fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
