8530846d0382989fb02072f67298e65fe448f787
qianyin <qianyin@newhope.cn>
2020-01-10 11:25:03 +0800

    modify PostAdmin

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post,Category,Tag
+from django.utils.html import format_html
+from django.urls import reverse
 # Register your models here.
 
 
@@ -21,3 +23,38 @@ class TagAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'create_time', 'operator'
+    ]
+    list_display_links = ['category', ]
+    search_fields = ['titile', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
