35811ca20dba27acf03575fca68db7786b42067a
qianyin <qianyin@newhope.cn>
2020-01-27 22:29:42 +0800

    7.2.1

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from .models import Post, Tag
+from .models import Post, Tag,Category
 from django.http import HttpResponse
 # Create your views here.
 
 
 def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    category = None
+
     if tag_id:
         try:
             tag = Tag.objects.get(id=tag_id)
@@ -15,9 +18,18 @@ def post_list(request, category_id=None, tag_id=None):
     else:
         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
         if category_id:
-            post_list = post_list.filter(category_id=category_id)
-
-    return render(request, 'blog/list.html', context={'post_list': post_list})
+            try:
+                category = Category.objects.get(id = category_id)
+            except Category.DoesNotExist:
+                post_list = []
+            else:
+                post_list = post_list.filter(category_id=category_id)
+    context = {
+        'category':category,
+        'tag':tag,
+        'post_list':post_list
+    }
+    return render(request, 'blog/list.html', context={'context': context})
 
 
 def post_detail(request, post_id=None):
